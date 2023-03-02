702333fbbd539c4c4880612d0aa939c3e067d4f2
mohit12141 <sahums1@rknec.edu>
2023-03-02 12:02:37 +0530

    Not completed because of paid api.

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,25 +1,37 @@
 import React, { useContext, useReducer } from "react";
 
-import {WeatherReducer} from './Reducer';
+import { WeatherReducer } from "./Reducer";
 
 const WeatherAPPContext = React.createContext();
 
-const WeatherAPPProvider = ({children}) => {
-    const [state,dispatch] = useReducer(WeatherReducer,{
-        city : '',
-        current : '',
-        daily : ''
-    })
-    
-    return <>
-        <WeatherAPPContext.Provider value={{state, dispatch}}>
-            {children}
-        </WeatherAPPContext.Provider>
+const WeatherAPPProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(WeatherReducer, {
+    city: {
+      city: "Nāgpur",
+      lat: "21.1539",
+      lng: "79.0831",
+      country: "India",
+      iso2: "IN",
+      admin_name: "Mahārāshtra",
+      capital: "",
+      population: "2405665",
+      population_proper: "2405665",
+    },
+    current: "",
+    daily: "",
+  });
+
+  return (
+    <>
+      <WeatherAPPContext.Provider value={{ state, dispatch }}>
+        {children}
+      </WeatherAPPContext.Provider>
     </>
+  );
 };
 
 export default WeatherAPPProvider;
 
 export const UseWeatherAPPContext = () => {
-    return useContext(WeatherAPPContext);
+  return useContext(WeatherAPPContext);
 };
